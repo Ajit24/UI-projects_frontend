30c0519191f39ccbea69354c219e80eda8bff663
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-05-23 12:06:02 +0530

    food recep page

---
+++ FOOD_recep/components/navbar.js
@@ -0,0 +1,16 @@
+
+function navbar() {
+
+    return ` <div id="navbar">
+    <h3><a href="search.html">Home</a></h3>
+    <div id="Options">
+    
+       
+        <h3><a href="reciepeOftheday.html">Recipe Of The Day</a></h3>
+        <h3><a href="latestReciepe.html">Latest Recipe.</a></h3>
+        <h3><a href="#">Signin</a></h3>
+        <h3><a href="#">Login</a></h3>
+    </div>`
+}
+
+export default navbar
